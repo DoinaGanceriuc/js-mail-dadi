a4e9f1079c68b1df1f56f2230d5884cce3850abd
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-14 16:57:39 +0200

    complete mail html and js file

--- Assets/Js/main.js
+++ Assets/Js/main.js
@@ -2,45 +2,51 @@
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo. Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.*/
 
 /* Strumenti
--prompt
 -array
 -input/button
 -querySelector/getElementById
+-addEventListener
+-for
+-if/else
 */
 
+// Array di email
 const emailUsers = ["paolo.rossi@gmail.com", "mario.bianchi@libero.it", "sara.bertozzi@gmail.com", "luca.bianchi@virgilio.it", "lucia@gmail.com", "marco94@live.it", "francesca85@gmail.com", "mario.gialli@outlook.com", "anna.verdi@gmail.com"];
 /* console.log(emailUsers.length);
 console.log(emailUsers[0]); */
 
+// selezionare elementi DOM
 const emailElement = document.getElementById("email");
 /* console.log(emailElement); */
 const buttonElement = document.getElementById("invia");
 /* console.log(buttonElement); */
+
+// Evento click
 buttonElement.addEventListener("click", function() {
 /* console.log("ho cliccato"); */
-const emailInserita = emailElement.value;
+const emailInserita = emailElement.value.toLowerCase();
 /* console.log(emailInserita); */
 
+//Definizione variabili
+let mailTrovata = false;
+let message;
+message = `La mail "${emailInserita}" NON risulta registrata. Accesso negato!`
+document.querySelector(".esito").innerHTML = message;
+
+// iterare tutte le mail dell'array
 for (let i = 0; i < emailUsers.length; i++) {
     const emailLista = emailUsers[i];
-    console.log(emailLista);
+    /* console.log(emailLista); */
 
-
+// condizione di verifica mail
     if (emailInserita == emailLista) {
-        console.log("accesso consentito");
-        document.getElementById("esito").innerHTML = "accesso consentito"
-
-    } else {
-        console.log("accesso negato");
-        document.getElementById("esito").innerHTML = "accesso negato"
+        mailTrovata = true;
+        /* console.log("accesso consentito"); */
+        message = `La mail "${emailInserita}" risulta registrata. Puoi proseguire!`
+        document.querySelector(".esito").innerHTML = message;
     }
-
-
-  
 }
-
-
-
+/*  console.log(mailTrovata); */
 })
 
 /* Gioco dei dadi
