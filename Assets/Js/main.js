011f8a97efbfc3bd651981efb16883f84dc930ed
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-10-14 15:04:31 +0200

    initial commit add html and js file

---
+++ Assets/Js/main.js
@@ -0,0 +1,15 @@
+/* Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo. Usiamo un input e un bottone per inserire la mail e poi mostriamo i risultati in pagina.*/
+
+/* Strumenti
+-prompt
+-array
+-input/button
+-querySelector/getElementById
+*/
+
+
+
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto. */
